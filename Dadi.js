95b6277caf34634ac42f3e84ef4f6fedc36da4a6
BrunoMaiolo <brunosj@hotmail.it>
2025-12-19 15:01:52 +0100

    creare un numero random da 1 a 6 sia per il player che computer

--- Dadi.js
+++ Dadi.js
@@ -1,1 +1,8 @@
 console.log("JS collegato")
+// Creare un numero random  da 1 a 6 sia per il player che per il computer
+const playerRoll = Math.floor(Math.random()*6) + 1;
+const computerRoll = Math.floor(Math.random() * 6) + 1;
+
+console.log("Giocatore:", playerRoll);
+console.log("Computer:", computerRoll);
+
