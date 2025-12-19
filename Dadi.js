d472bb8e35e5c7cdb5543a94b35ee9f09b15a72c
BrunoMaiolo <brunosj@hotmail.it>
2025-12-19 15:06:10 +0100

    - Se giocatore > computer  vince giocatore - Se computer > giocatore vince computer - Altrimenti pareggio

--- Dadi.js
+++ Dadi.js
@@ -1,8 +1,17 @@
 console.log("JS collegato")
-// Creare un numero random  da 1 a 6 sia per il player che per il computer
+// Creare un numero random  da 1 a 6 sia per il player che per il computer e stampare
 const playerRoll = Math.floor(Math.random()*6) + 1;
 const computerRoll = Math.floor(Math.random() * 6) + 1;
 
 console.log("Giocatore:", playerRoll);
 console.log("Computer:", computerRoll);
+
+// - Se giocatore > computer  vince giocatore - Se computer > giocatore vince computer - Altrimenti pareggio
+if (playerRoll > computerRoll) {
+  console.log("Hai vinto!");
+} else if (computerRoll > playerRoll) {
+  console.log("Ha vinto il computer!");
+} else {
+  console.log("Pareggio!");
+}
 
