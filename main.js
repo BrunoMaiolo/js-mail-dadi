5f880a9904307382e1796ab28c09ceae3fcf2e27
BrunoMaiolo <brunosj@hotmail.it>
2025-12-19 14:32:36 +0100

    lista mail invitati

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 console.log(" JS collegato")
+
+//Lista mail degli invitati
+const invitedEmails = [
+    "bruno@hotmail.it",
+    "luca@hotmail.it",
+    "mario@hotmail.it",
+    "andrea@hotmail.it",
+    "luigi@hotmail.it"
+];
